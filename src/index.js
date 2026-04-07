95aa3f877bb7bb522ecab0f2f209d500b179703b
AyushDua-07 <duaayush121@gmail.com>
2026-04-07 12:38:04 -0700

    Change server listen address to '0.0.0.0'

--- src/index.js
+++ src/index.js
@@ -38,6 +38,6 @@ app.use('/api/admin', adminRoutes);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
